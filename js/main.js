e5e80a1e1469cadebdbb6a9025dffb3983b2d9b4
davidluz <davidmelo8@gmail.com>
2016-11-21 15:30:31 -0200

    v 0.0.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Informações do Storyboard
 var numeroDeTelas = 0;
 var telasAtualizadas = 0;
 var telasNovas = 0;
@@ -7,8 +8,38 @@ var numeroDeCaracteres = 0;
 var numeroDeLaudas = 0;
 var dataDeProducao = "00/00/00";
 var versao = 0;
-var telas = [];
+var telasSelecionadas = [];
 
+
+$(document).ready(function(){
+
+//Teste para gerar tela
+var teste = new GeradorDeTela(1,2,3,4,5);
+console.log(teste.conteudo);
+
+$( function() {
+  $( ".telas img" ).draggable({
+    	revert: true
+    });
+  } );	
+
+
+$( ".telas-selecionadas" ).droppable({
+     drop: function( event, ui ) {
+     var draggableId = ui.draggable.attr("id");
+     console.log(ui.draggable);
+     var primeiroFlho = $(this).first();
+    $(primeiroFlho).html("<div class='tela-editavel'><img  width='100px' height:'51px' src= "+ui.draggable.attr("src")+"> </div>");
+   
+     }
+});
+
+});
+
+
+
+
+//Classe geradora de Tela 
 var GeradorDeTela = function(id,conteudo,descricaoImagem, orientacoes,anexos){
   this.id = id;
   this.conteudo = conteudo;
@@ -16,44 +47,3 @@ var GeradorDeTela = function(id,conteudo,descricaoImagem, orientacoes,anexos){
   this.orientacoes = orientacoes;
   this.anexos = anexos; 
 }
-
-$(document).ready(function(){
-
-var teste = new GeradorDeTela(1,2,3,4,5);
-console.log(teste.conteudo);
-
-  $( function() {
-    $( ".telas img" ).draggable({
-    	revert: true
-    });
-  } );	
-
-	$('.texto-editavel').attr('contenteditable','true');
-});
-
-
-function insertLine(){
-	$('#homologacao').append('<tr><td><p>item 1</p></td><td><p>item 1</p></td></tr>');
-
-}
-
-$("button").click(function(){
-
-
-  $("#homologacao").table2excel({
-    // exclude CSS class
-    exclude: ".noExl",
-    name: "Worksheet Name",
-    filename: "homologacao.xls" //do not include extension
-  });
-
-});
-
-$( "#storyboard" ).droppable({
-        drop: function( event, ui ) {
-        var draggableId = ui.draggable.attr("id");
-        console.log(draggableId);
-       $( "#storyboard" ).append(draggableId);
-
-      }
-    });
